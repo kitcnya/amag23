5b9b05bd8d0a30fda35aa8584bf3e8b578d214e6
kitcnya <kitcnya@outlook.com>
2024-03-03 03:29:41 +0900

    modify paramters (add more wait bitween aim on/off)

--- source.c
+++ source.c
@@ -247,11 +247,11 @@ struct keymac_element {
 #define KMSEQ(name)	static struct keymac_element name[]
 
 KMSEQ(machine_gun) = {
-	KM_REPEAT(100),
+	KM_REPEAT(80),
 	KM_KEY_TAP(KC_BTN1),
 	KM_INTERVAL(60),
 	KM_KEY_TAP(KC_R),
-	KM_INTERVAL(100),
+	KM_INTERVAL(110),
 	KM_KEY_TAP(KC_R),
 };
 
